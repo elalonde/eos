eb7c4eb8f62a431a24f42bae0eb75102a53a8f02
Eric Lalonde <eric@lalonde.com>
2026-06-27 12:13:47 -0700

    Switch to table-driven cursor setup

--- loader.s
+++ loader.s
@@ -1,14 +1,20 @@
-global loader                   ; the entry symbol for ELF
+global eos                      ; the entry symbol for ELF
 
 MAGIC_NUMBER equ 0x1BADB002     ; define the magic number constant
 FLAGS        equ 0x0            ; multiboot flags
 CHECKSUM     equ -MAGIC_NUMBER  ; calculate the checksum
                                 ; (magic number + checksum + flags should equal 0)
 PRINT_START_OFFSET equ 0xB8000 + 15*80*2
-VGA_CRTC_IDX_PORT equ 0x3D4          ; VGA CRTC data port
-VGA_CRTC_DAT_PORT equ 0x3D5		    ; VGA CRTC index port
-CURS_START_IDX equ 0x0A         ; cursor start register index
-CURS_END_IDX equ 0x0B           ; cursor end register index
+VGA_CRTC_IDX_PORT equ 0x3D4     ; VGA CRTC index port
+VGA_CRTC_DAT_PORT equ 0x3D5		; VGA CRTC data port
+
+section .rodata
+                                ; blinking cursor (register, value) pairs
+	cursor db 0x0E, 0x04        ; set cursor location high bits
+           db 0x0F, 0xBC        ; set cursor location low bits
+		   db 0x0A, 0x00        ; set cursor start scanline
+		   db 0x0B, 0x0F        ; set cursor end scanline
+    cursor_len equ $-cursor
 
 section .data
 	msg db "Hello, Eric."
@@ -20,7 +26,8 @@ align 4                         ; the code must be 4 byte aligned
     dd FLAGS                    ; the flags,
     dd CHECKSUM                 ; and the checksum
 
-loader:                         ; the loader label (defined as entry point in linker script)
+eos:                            ; the entry label (defined as entry point in linker script)
+
 	mov ecx, 0
 .printmsg:
 	mov bl, [msg + ecx]
@@ -30,30 +37,17 @@ loader:                         ; the loader label (defined as entry point in li
 	cmp ecx, msg_len
 	jne .printmsg
 
+	mov ecx, 0
+.printcursor:
 	mov dx, VGA_CRTC_IDX_PORT
-	mov al, 0x0E
-	out dx, al      ; latch location high bits
+	mov al, [cursor+ecx]
+	out dx, al                 ; latch register
 	mov dx, VGA_CRTC_DAT_PORT
-	mov al, 0x4
-	out dx, al      ; set location high bits
-	mov dx, VGA_CRTC_IDX_PORT
-	mov al, 0x0F
-	out dx, al      ; latch location low bits
-	mov dx, VGA_CRTC_DAT_PORT
-	mov al, 0xBC
-	out dx, al      ; set location low bits
-	mov dx, VGA_CRTC_IDX_PORT
-	mov al, 0x0A
-	out dx, al      ; latch cursor start register
-	mov dx, VGA_CRTC_DAT_PORT
-	mov al, 0x00
-	out dx, al      ; set cursor start scanline
-	mov dx, VGA_CRTC_IDX_PORT
-	mov al, 0x0B
-	out dx, al      ; latch cursor end register
-	mov dx, VGA_CRTC_DAT_PORT
-	mov al, 0x0F
-	out dx, al      ; set cursor end scanline
+	mov al, [cursor+ecx+1]
+	out dx, al                 ; write data
+	add ecx, 2 ; next pair
+	cmp ecx, cursor_len
+	jne .printcursor
 
 .loop:
     jmp .loop                   ; loop forever
