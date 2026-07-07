f135266c2101e00bc79d1bd1fb634d2c975b6c30
Eric Lalonde <eric@lalonde.com>
2026-07-07 15:27:03 -0700

    Continue register-heritage-honoring rewrite
    - rewrite fb_scroll, fb_newline

--- src/loader.s
+++ src/loader.s
@@ -13,15 +13,16 @@ KERNEL_STACK_SIZE equ 4096      ; size of stack in bytes
 BLACK_TEXT equ 0x07
 HEX_GT_TEN_ASCII_OFFSET equ 0x37
 HEX_LT_TEN_ASCII_OFFSET equ 0x30
-LINE_LEN equ 0x50
+LINE_LEN_BYTES equ 0xA0
 BL_REPORT_INDENT_LEN equ 0x4
-FB_SCROLL_BOUNDARY equ 0x7D0
+FB_SCROLL_BOUNDARY_ADDR equ 0xB8FA0
 CRTC_HIGH_BYTE equ 0x0E
 CRTC_LOW_BYTE equ 0x0F
+FB_SCROLL_LEN_BYTES equ 0xF00
 
 section .bss
 	align 4                 ; align at 4 bytes
-	fb_indent_len resd 1
+	fb_indent_bytes resd 1
 	fb_mem_addr resd 1
 kernel_stack:
 	resb KERNEL_STACK_SIZE  ; reserve stack for the kernel
@@ -52,12 +53,12 @@ load_eos:
 	mov esp, kernel_stack + KERNEL_STACK_SIZE
 
 	call crtc_read_fb_cell
-	;call fb_skip_ln
+	call fb_newline
 
 	;mov edx, welcome
 	;mov eax, welcome_len
 	;call prn_msg
-	;call fb_skip_ln
+	;call fb_newline
 
 	;call prn_bl_rpt
 	;call prn_cursor
@@ -68,6 +69,49 @@ load_eos:
 	hlt
 	jmp .hang
 
+
+fb_scroll:
+	push esi
+	push edi
+	mov esi, FB_MMIO_ADDR+LINE_LEN_BYTES
+	mov edi, FB_MMIO_ADDR
+	mov ecx, FB_SCROLL_LEN_BYTES
+	rep movsb
+	; blank last line
+	mov edi, FB_SCROLL_BOUNDARY_ADDR-LINE_LEN_BYTES
+	mov al, ' '
+	mov ah, BLACK_TEXT
+	mov ecx, LINE_LEN_BYTES
+	shr ecx, 1 ; word count
+	rep stosw
+	pop edi
+	pop esi
+	ret
+
+fb_newline:
+	push ebx
+	mov eax, [fb_mem_addr]
+	push eax
+	; bytes since beginning of fb
+	sub eax, FB_MMIO_ADDR
+	mov ebx, LINE_LEN_BYTES
+	xor edx, edx
+	div ebx
+	sub ebx, edx ; bytes to next row
+	pop eax
+	add eax, ebx ; new cursor addr
+	cmp eax, FB_SCROLL_BOUNDARY_ADDR
+	jb .skip_scroll
+	push eax
+	call fb_scroll
+	pop eax
+	; move cursor back on screen
+	sub eax, LINE_LEN_BYTES
+.skip_scroll:
+	add eax, [fb_indent_bytes]
+	mov [fb_mem_addr], eax
+	pop ebx
+	ret
 
 crtc_read_fb_cell:
 	; latch and read cursor location high
